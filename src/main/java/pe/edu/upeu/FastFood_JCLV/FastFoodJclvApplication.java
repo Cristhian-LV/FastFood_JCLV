b2f1165680b574a450fa474cb5e56b0fab8bb7ce
Cristhian-LV <thiancris4@gmail.com>
2025-09-16 15:05:53 -0500

    16_setiembre_2025

---
+++ src/main/java/pe/edu/upeu/FastFood_JCLV/FastFoodJclvApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.FastFood_JCLV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodJclvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodJclvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upeu/FastFood_JCLV/FastFoodJclvApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.FastFood_JCLV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodJclvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
